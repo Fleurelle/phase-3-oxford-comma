31b0b0d750713c38473534007a929ffd565f1719
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-08-21 23:36:49 -0400

    commit tree 134988918a68cc51806cb17fd756d4391dcd5796

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+# require "pry"
 def oxford_comma(array)
 
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    lastElement = array.pop()
+    combinedArray = array.join(", ")
+   return combinedArray << ", and #{lastElement}"
+  else
+    array.join(" , ")
+  end
 end
